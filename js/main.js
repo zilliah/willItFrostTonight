ed24c90dde8cf9b0089bd3540bcc9c318e6ff5c5
Kerry Smart <kerry.z.smart@gmail.com>
2022-04-19 16:54:08 -0400

    basic frost check working, for current day only

--- js/main.js
+++ js/main.js
@@ -1,24 +1,26 @@
-document.querySelector('button').addEventListener('click', getWeather);
+document.querySelector('button').addEventListener('click', getLocation);
 const answer = document.querySelector("h2");
 let answerLoc = "";
 
 const url = "https://api.openweathermap.org/";
 
 
-function getWeather() {
-  let location = getLocation();
+function getWeather(lat, lon) {
+  console.log(lat, lon);
+  // let weatherUrl = `${url}data/2.5/forecast/daily?lat=${location.lat}&long=${location.lon}&cnt=2&units=metric&appid=${key}`;
 
-  let weatherUrl = `${url}data/2.5/forecast/daily?lat=${location.lat}&long=${location.lon}&cnt=2&units=metric&appid=${key}`;
 
-  console.log(weatherUrl);
+  let weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={current,minutely,hourly,alerts}&units=metric&appid=${key}`;
+
 
   fetch(weatherUrl)
     .then(res => res.json())
     .then(data => {
       console.log(data);
+      console.log(data.daily);
 
-      let minTemp = data.list.list.temp.min;
-      if (minTemp > 0) answer.textContent = `YES, there will be ${answerLoc}`;
+      let minTemp = data.daily[0].temp.min;
+      if (minTemp <= 0) answer.textContent = `YES, there will be ${answerLoc}`;
       else answer.textContent = `NO, there will be no ${answerLoc}`;
 
     })
@@ -33,25 +35,17 @@ function getLocation(){
   let city = document.querySelector('input').value;
   if (!city) city = "Ottawa";
   const country = document.querySelector("select").value;
-
   let locationUrl = `${url}geo/1.0/direct?q=${city.toLowerCase()},${country}&appid=${key}`;
-  let coords = {};
-  
-  
+
   fetch(locationUrl)
   .then(res => res.json())
   .then(data => {
-    console.log(data);
-    
     //ok taking the first element is sketchy, may return multiple results
-    //can refine with a selection at some point maybe
+    //should redo with selector for which result to show data for
     answerLoc = `frost tonight in ${data[0].name}, ${data[0].state}.`;
-
-    coords.lat = data[0].lat;
-    coords.lon = data[0].lon;
+    getWeather(data[0].lat, data[0].lon);
     })
   .catch(err => {
       console.log(`error ${err}`);
   });
-  return coords;
 }

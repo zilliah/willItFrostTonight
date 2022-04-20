98037ffc53903b6998e540d8e82eacd0911b958c
Kerry Smart <kerry.z.smart@gmail.com>
2022-04-19 20:09:11 -0400

    add basic styling

--- js/main.js
+++ js/main.js
@@ -1,41 +1,38 @@
 document.querySelector('button').addEventListener('click', getLocation);
 const answer = document.querySelector("h2");
 const tomorrow = document.querySelector("h3");
+const body = document.querySelector("body");
 let answerLoc = "";
 
 const url = "https://api.openweathermap.org/";
 
 
 function getWeather(lat, lon) {
-  console.log(lat, lon);
-  // let weatherUrl = `${url}data/2.5/forecast/daily?lat=${location.lat}&long=${location.lon}&cnt=2&units=metric&appid=${key}`;
-
-
   let weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={current,minutely,hourly,alerts}&units=metric&appid=${key}`;
-
 
   fetch(weatherUrl)
     .then(res => res.json())
     .then(data => {
-      console.log(data);
-      console.log(data.daily);
-
       let minTemp = data.daily[0].temp.min;
-      if (minTemp <= 0) answer.textContent = `YES, there will be ${answerLoc}`;
-      else answer.textContent = `NO, there will be no ${answerLoc}`;
+      if (minTemp <= 0) {
+        answer.textContent = `YES, there will be ${answerLoc}`;
+        body.classList.add("frost");
+        body.classList.remove("sunny");
+      }
+      else {
+        answer.textContent = `NO, there will be no ${answerLoc}`;
+        body.classList.add("sunny");
+        body.classList.remove("frost");
+      } 
 
       let tomorrowMin = data.daily[1].temp.min;
       if (tomorrowMin <= 0) tomorrow.textContent = "There will be frost tomorrow night.";
       else tomorrow.textContent = "No frost tomorrow night.";
-
-
     })
     .catch(err => {
       console.log(`error ${err}`);
   });
-
 }
-
 
 function getLocation(){
   let city = document.querySelector('input').value;
